1506a04c82cd1af26fcc46b09a3d056aacb38bc1
Federico Jaramillo Martínez <federicojaramillom@gmail.com>
2019-08-20 18:23:06 -0500

    Use mkcert for self signed certificates

--- {{ cookiecutter.docker_dir }}/caddy/docker-entrypoint.sh
+++ {{ cookiecutter.docker_dir }}/caddy/docker-entrypoint.sh
@@ -4,12 +4,9 @@ set -eo pipefail
 if [[ ! -z "$CADDY_CERT" && ! -z "$CADDY_PRIVKEY" ]] && [[ ! -f $CADDY_CERT || ! -f $CADDY_PRIVKEY ]];
 then
     echo "Generating new self-sign certificates" >&2
-    mkdir -p /srv/certs
-    printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth" > `dirname $(mktemp -u)`/openssl.conf
-    openssl req -x509 -out "$CADDY_CERT" -keyout "$CADDY_PRIVKEY" \
-        -newkey rsa:2048 -nodes -sha256 \
-        -subj '/CN=localhost' -extensions EXT -config `dirname $(mktemp -u)`/openssl.conf
-    rm `dirname $(mktemp -u)`/openssl.conf
+    mkdir -p "$(dirname "$CADDY_CERT")"
+    mkdir -p "$(dirname "$CADDY_PRIVKEY")"
+    mkcert -cert-file "$CADDY_CERT" -key-file "$CADDY_PRIVKEY" ${CADDY_DOMAIN%:*}
 fi
 
 exec "$@"
